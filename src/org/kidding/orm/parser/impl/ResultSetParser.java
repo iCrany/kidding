f9d9fa95dd2585f32a7cea5374a53d2251723241
iCrany <crany1992@gmail.com>
2014-12-12 10:13:50 +0800

    ResultSetParser 添加只返回某些字段的功能

--- src/org/kidding/orm/parser/impl/ResultSetParser.java
+++ src/org/kidding/orm/parser/impl/ResultSetParser.java
@@ -37,17 +37,25 @@ public class ResultSetParser<T extends POJO> {
 
     /**
      * 将 resultSet 对象中的数据组织成 List<T> 形式返回
+     * @param classType 类的类型
      * @param rs 结果对象集
+     * @param params 需要返回的字段，尽量填写，避免不必要的数据传输
      * @return
      * @throws SQLException
      */
-    public List<T> parseList(Class<T> classType , ResultSet rs) throws SQLException, IllegalAccessException, InstantiationException, InvocationTargetException {
+    public List<T> parseList(Class<T> classType , ResultSet rs,String... params) throws SQLException, IllegalAccessException, InstantiationException, InvocationTargetException {
         List<T> result = new ArrayList<T>();
-        List<String> keyList = entityParser.getAllKey(classType.newInstance());
-        System.out.println("正在 parseList ... ");
+        List<String> keyList;
+        if(0 == params.length) {
+            keyList = entityParser.getAllKey(classType.newInstance());
+        }else{
+            keyList = new ArrayList<String>(params.length);
+            for(String param : params){
+                keyList.add(param);
+            }
+        }
         while(rs.next()){
             T entity = classType.newInstance();
-
             for(String key : keyList){
                 if(null != rs.getObject(key)){
                     Object value = rs.getObject(key);
@@ -63,14 +71,23 @@ public class ResultSetParser<T extends POJO> {
      * 获取单个的实体类
      * @param classType 类的类型
      * @param rs 结果集合
+     * @param params 需要返回的字段，尽量填写，避免不必要的数据传输
      * @return
      * @throws SQLException
      * @throws IllegalAccessException
      * @throws InstantiationException
      */
-    public T parse(Class<T> classType, ResultSet rs) throws SQLException, IllegalAccessException, InstantiationException, InvocationTargetException {
+    public T parse(Class<T> classType, ResultSet rs,String... params) throws SQLException, IllegalAccessException, InstantiationException, InvocationTargetException {
         T entity = classType.newInstance();
-        List<String> keyList = entityParser.getAllKey(entity);
+        List<String> keyList;
+        if(0 == params.length) {
+            keyList = entityParser.getAllKey(entity);
+        }else{
+            keyList = new ArrayList<String>(params.length);
+            for(String param : params){
+                keyList.add(param);
+            }
+        }
 
         if(rs.next()){
             for(String key : keyList){
